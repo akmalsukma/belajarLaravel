84c79a06634090b48e8c39f23a0576d1ddc08e69
Akmal Sukma <akmal.awe2@gmail.com>
2020-07-26 16:08:05 +0700

    Menambahkan Routing dengan parameter dan reguler expression

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,17 @@ Route::get('/portfolio',function(){
     return view('portfolio');
 });
 
+Route::get('/artikel/{judul}',function($judul){
+    return "Artikel ini Berjudul : $judul";
+});
+
+Route::get('/artikel/{judul}/{kategori}',function($judul,$kategori){
+    return "Artikel ini Berjudul : $judul <br> Kategori : $kategori";
+});
+
+Route::get('/user/{id_user}',function($id_user){
+    return "ID user : $id_user";
+})->where('id_user','[A-Z]{3}[1-9]+');
 
 Route::fallback(function (){
     return "Halaman yang anda tuju tidak ada";
